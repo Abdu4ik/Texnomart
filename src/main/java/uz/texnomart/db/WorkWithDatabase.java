779ea4499669f49c741bf4c1a2941c752e7c912c
abdullokhayrulloev <strengthnumberone@gmail.com>
2022-10-29 14:38:21 +0500

    PCategory list method moved from UserService to WorkingWithDatabase class

--- src/main/java/uz/texnomart/db/WorkWithDatabase.java
+++ src/main/java/uz/texnomart/db/WorkWithDatabase.java
@@ -1,7 +1,11 @@
 package uz.texnomart.db;
 import uz.texnomart.entity.Advertisement;
+import uz.texnomart.entity.Category;
 import uz.texnomart.entity.TelegramUser;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 import static uz.texnomart.container.Container.*;
 
 public class WorkWithDatabase {
@@ -79,5 +83,30 @@ public class WorkWithDatabase {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public static List<Category> parentCategoryList(){
+        List<Category> categoryList = new ArrayList<>();
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+             Statement statement = connection.createStatement()
+        ) {
+
+            String query = """
+                              select * from category where parent_id = null;
+                    """;
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt(1);
+                String name = resultSet.getString("name");
+                int parent_id = resultSet.getInt("parent_id");
+                boolean is_deleted = resultSet.getBoolean("is_deleted");
+                categoryList.add(new Category(id, name, parent_id, is_deleted));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return categoryList;
     }
 }
--- src/main/java/uz/texnomart/service/UserService.java
+++ src/main/java/uz/texnomart/service/UserService.java
@@ -10,29 +10,4 @@ import static uz.texnomart.container.Container.*;
 
 public class UserService {
 
-    public static List<Category> parentCategoryList(){
-        List<Category> categoryList = new ArrayList<>();
-        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
-             Statement statement = connection.createStatement()
-        ) {
-
-            String query = """
-                              select * from category where parent_id = null;
-                    """;
-            ResultSet resultSet = statement.executeQuery(query);
-
-            while (resultSet.next()) {
-                int id = resultSet.getInt(1);
-                String name = resultSet.getString("name");
-                int parent_id = resultSet.getInt("parent_id");
-                boolean is_deleted = resultSet.getBoolean("is_deleted");
-                categoryList.add(new Category(id, name, parent_id, is_deleted));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return categoryList;
-    }
-
 }
